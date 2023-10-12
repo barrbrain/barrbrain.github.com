e32433e8751f3386ffea37a4d76f8006b8c592e7
David Michael Barr <b@rr-dav.id.au>
2023-10-13 01:11:40 +0900

    towers: Translate problem init to C++

--- towers/towers.cc
+++ towers/towers.cc
@@ -109,3 +109,73 @@ class Queue {
     return raw[max].empty();
   }
 };
+
+static const uint32_t WIDTH = 200;
+static const uint32_t HEIGHT = 200;
+static const uint32_t USERS = 10000;
+static const uint32_t TOWERS = 200;
+static const uint8_t LIMIT = 100;
+
+#include <cstdlib>
+
+class Towers {
+  private:
+  uint32_t length;
+  uint8_t x[TOWERS], y[TOWERS], c[TOWERS];
+  uint8_t limit;
+
+  public:
+  Towers(uint32_t len, uint8_t lim, uint32_t width, uint32_t height) : length(len), limit(lim) {
+    for (uint32_t n = 0; n < length; n++) {
+      x[n] = (rand() >> 8) * width / (RAND_MAX >> 8);
+      y[n] = n;
+      c[n] = 0;
+    }
+  }
+
+  bool const is(uint8_t x, uint8_t y) {
+    for (uint32_t n = 0; n < length; n++) {
+      if (this->x[n] == x && this->y[n] == y) return true;
+    }
+    return false;
+  }
+};
+
+class Users {
+  private:
+  uint32_t length;
+  uint8_t t[USERS], x[USERS], y[USERS];
+  uint16_t n[WIDTH * HEIGHT];
+
+  public:
+  Users(uint32_t len, uint32_t width, uint32_t height, Towers& t) : length(len) {
+    for (uint32_t n = 0; n < length;) {
+      uint8_t x = (rand() >> 8) * width / (RAND_MAX >> 8);
+      uint8_t y = (rand() >> 8) * height / (RAND_MAX >> 8);
+      uint32_t i = x + width * y;
+      if (this->n[i] != 0 || t.is(x, y)) continue;
+      this->n[i] = ++n;
+    }
+  }
+
+  bool const is(uint8_t x, uint8_t y, uint32_t width) {
+    return n[x + width * y] != 0;
+  }
+};
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  uint32_t width = WIDTH / 2;
+  uint32_t height = HEIGHT / 2;
+  Towers t(TOWERS / 2, LIMIT / 2, width, height);
+  Users u(USERS / 2, width, height, t);;
+  for (uint8_t y = 0; y < height; y++) {
+    for (uint8_t x = 0; x < width; x++) {
+      cout << (t.is(x,y) ? "T" : u.is(x, y, width) ? "u" : " ");
+    }
+    cout << endl;
+  }
+  return 0;
+}
